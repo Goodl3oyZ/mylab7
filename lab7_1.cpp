22944280b5c7391ca3e84ce9fe393fe70913579b
neokarn <34811101+neokarn@users.noreply.github.com>
2021-11-29 16:41:29 +0700

    Create lab7_1.cpp

---
+++ lab7_1.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+string func1(string x){
+	int i = 0, L = x.size();
+	string y = "";
+	while(i < L){
+		y += x[L-i-1];
+		i++;
+	}
+	return y;
+}
+
+string func2(string x){
+	int i = 0, L = x.size();
+	string y = "";
+	while(i < L){
+		y += toupper(x[i]);
+		i++;
+	}
+	return y;	
+}
+
+string func3(string x){
+	int i = 0, L = x.size();
+	string y = "";
+	while(i < L){
+		y += tolower(x[i]);
+		i++;
+	}
+	return y;	
+}
+
+int main(){
+    cout >> "Input text: "
+    cout >> "Reversed text: "
+    cout >> "Palindrome: "
+    return 0;
+}
