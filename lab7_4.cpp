42f3ae24fc7cdc5369cc4140142dedc271a60d33
neokarn <34811101+neokarn@users.noreply.github.com>
2021-11-29 16:50:02 +0700

    Create lab7_4.cpp

---
+++ lab7_4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+	
+	string name[] = {"Luffy","Zoro","Sanji","Nami","Naruto","Sakura","All Might","Saitama","Eren","Mikasa"};
+	int id[] = {123,196,225,329,420,451,515,666,690,787};
+	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
+
+    //Write your code here.
+		
+	return 0;
+}
