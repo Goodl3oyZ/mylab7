f0f56666cd0a928eb21aa6e7e3a0e8e3fa446eba
neokarn <34811101+neokarn@users.noreply.github.com>
2021-11-29 16:49:38 +0700

    Create lab7_3.cpp

---
+++ lab7_3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+  cout << adiff(180,270);
+  cout << adiff(210,45);
+  cout << adiff(0,360);
+  cout << adiff(10,350);
+  cout << adiff(95,260);
+  cout << adiff(90,-90);
+  cout << adiff(1000,280);
+  cout << adiff(60,244);
+}
