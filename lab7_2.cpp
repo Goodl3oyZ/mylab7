dadeb62b0e761f55574348327096428ab099b242
Goodl3oyZ <panudetzx2@gmail.com>
2022-12-13 16:10:23 +0700

    sedla 2 dont forgot i again

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -4,11 +4,29 @@ using namespace std;
 
 int main(){
 	
-	int i = 0, P[5000], k, result;
+	int i = 0, P[5000], k, result=0;
 	while(i < 5000){
 		P[i] = 2*i+1;
 		i++;
 	}
-	
+	cout<<"Please input k: ";
+	cin>>k;
+	if(k>0){
+		int i=0;
+		if(i==0){
+				result=0;}
+		while(i<5000){
+			if(P[i]%k==0){
+				result=result-P[i];
+			}else{
+				result=result+P[i];
+			}
+			i++;
+		}
+		cout<<"Result = "<<result;
+	}
+	else{
+		cout<<"Invalid input!!!";
+	}
 	return 0;
 }
